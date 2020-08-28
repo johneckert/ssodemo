0856bbfb143ef66bfdf752c151fce9d3f92ad9b6
John Eckert <johnteckert@gmail.com>
2020-08-28 16:45:30 -0400

    changed url

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NgModule } from "@angular/core";
 
-import { MatButtonModule } from '@angular/material/button';
-import { MatListModule } from '@angular/material/list';
-import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from "@angular/material/button";
+import { MatListModule } from "@angular/material/list";
+import { MatToolbarModule } from "@angular/material/toolbar";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
 
-import { MsalModule, MsalInterceptor } from '@azure/msal-angular';
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
-import { ProfileComponent } from './profile/profile.component';
-import { HomeComponent } from './home/home.component';
+import { MsalModule, MsalInterceptor } from "@azure/msal-angular";
+import { HTTP_INTERCEPTORS, HttpClientModule } from "@angular/common/http";
+import { ProfileComponent } from "./profile/profile.component";
+import { HomeComponent } from "./home/home.component";
 
-const isIE = window.navigator.userAgent.indexOf('MSIE ') > -1 || window.navigator.userAgent.indexOf('Trident/') > -1;
+const isIE =
+  window.navigator.userAgent.indexOf("MSIE ") > -1 ||
+  window.navigator.userAgent.indexOf("Trident/") > -1;
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ProfileComponent,
-    HomeComponent
-  ],
+  declarations: [AppComponent, ProfileComponent, HomeComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -31,38 +29,37 @@ const isIE = window.navigator.userAgent.indexOf('MSIE ') > -1 || window.navigato
     MatButtonModule,
     MatListModule,
     AppRoutingModule,
-    MsalModule.forRoot({
-      auth: {
-        clientId: '2ac878ba-5deb-4ec9-811d-2ecabf903715',
-        authority: 'https://login.microsoftonline.com/be2189e0-840a-419c-a4da-bd793d03396a',
-        redirectUri: 'http://localhost:4200/',
+    MsalModule.forRoot(
+      {
+        auth: {
+          clientId: "2ac878ba-5deb-4ec9-811d-2ecabf903715",
+          authority:
+            "https://login.microsoftonline.com/be2189e0-840a-419c-a4da-bd793d03396a",
+          redirectUri: "https://johneckert.github.io/ssodemo/", //'http://localhost:4200/',
+        },
+        cache: {
+          cacheLocation: "localStorage",
+          storeAuthStateInCookie: isIE, // set to true for IE 11
+        },
       },
-      cache: {
-        cacheLocation: 'localStorage',
-        storeAuthStateInCookie: isIE, // set to true for IE 11
-      },
-    },
-    {
-      popUp: !isIE,
-      consentScopes: [
-        'user.read',
-        'openid',
-        'profile',
-      ],
-      unprotectedResources: [],
-      protectedResourceMap: [
-        ['https://graph.microsoft.com/v1.0/me', ['user.read']]
-      ],
-      extraQueryParameters: {}
-    })
+      {
+        popUp: !isIE,
+        consentScopes: ["user.read", "openid", "profile"],
+        unprotectedResources: [],
+        protectedResourceMap: [
+          ["https://graph.microsoft.com/v1.0/me", ["user.read"]],
+        ],
+        extraQueryParameters: {},
+      }
+    ),
   ],
   providers: [
     {
       provide: HTTP_INTERCEPTORS,
       useClass: MsalInterceptor,
-      multi: true
-    }
+      multi: true,
+    },
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
